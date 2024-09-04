a670d655d123640c79e3be148a239a711e767192
Suraj Gupta <sgsurajgupta4@gmail.com>
2024-09-04 15:24:48 +0530

    Update score.py

--- score.py
+++ score.py
@@ -10,6 +10,7 @@ class Score(Turtle):
         self.goto(0, 370)
         self.hideturtle()
         self.update_scoreboard()
+        self.reset()
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}",align=ALIGNMENT,font=FONT)
@@ -19,6 +20,8 @@ class Score(Turtle):
             self.clear()
             self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+     def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
